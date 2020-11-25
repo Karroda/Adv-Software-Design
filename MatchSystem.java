6dfcb5a50f3c253b8366eca9f5044117a696e524
shashd <1065709724@qq.com>
2020-11-25 19:09:11 +0800

    correct words

--- MatchSystem.java
+++ MatchSystem.java
@@ -107,7 +107,7 @@ public class MatchSystem {
     }
 
     /**
-     * Stimulate transferring requestInfo, responder payment, feedback
+     * Simulate transferring requestInfo, responderPayment, feedback
      * @param args
      */
     public static void main(String[] args) {
